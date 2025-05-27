4590de4844d816fb5518144231b60358405e3317
Himanshu Mendapara <himanshumendapra@gmail.com>
2025-05-27 08:34:58 +0530

    feat:  video functionality added

--- apps/web/components/pages/home/home.tsx
+++ apps/web/components/pages/home/home.tsx
@@ -1,3 +1,33 @@
+"use client";
+import { useWindowSize } from "usehooks-ts";
+
+import { useEffect, useRef } from "react";
+
 export function Home() {
-  return <div>Konekt</div>;
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const { width, height } = useWindowSize();
+
+  useEffect(() => {
+    (async () => {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+      });
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream;
+        videoRef.current.play();
+      }
+    })();
+  }, []);
+
+  return (
+    <video
+      ref={videoRef}
+      height={height * 0.5}
+      width={width * 0.5}
+      autoPlay
+      muted
+      className="rounded-xl"
+    />
+  );
 }
