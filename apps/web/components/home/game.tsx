73dfba5d94b81be5332701c4c4aaef841ac47bd9
Himanshu Mendapara <himanshumendapra@gmail.com>
2025-06-10 01:08:34 +0530

    feat: added chess board in the game

--- apps/web/components/home/game.tsx
+++ apps/web/components/home/game.tsx
@@ -1,7 +1,19 @@
+import { Chessboard } from "react-chessboard";
+import { useWindowSize } from "usehooks-ts";
+
 export function Game() {
+  const { height, width } = useWindowSize();
+
   return (
-    <div className="w-full bg-secondary text-lg font-semibold rounded-2xl h-10/11 flex items-center justify-center">
-      Game section comming soon!
+    <div className="p-4 [&>div]:w-min! w-auto items-center h-auto flex bg-secondary text-lg font-semibold rounded-2xl justify-center">
+      <Chessboard
+        arePiecesDraggable
+        showBoardNotation
+        showPromotionDialog
+        boardOrientation="black"
+        animationDuration={10000}
+        boardWidth={height * 0.8 < width - 100 ? height * 0.8 : width - 100}
+      />
     </div>
   );
 }
