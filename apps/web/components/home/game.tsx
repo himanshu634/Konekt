5f66f72114fe8611208798a3af77b2417e4bef6c
Himanshu Mendapara <himanshumendapra@gmail.com>
2025-06-10 01:12:24 +0530

    minor fixes

--- apps/web/components/home/game.tsx
+++ apps/web/components/home/game.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@konekt/ui/utils";
 import { Chessboard } from "react-chessboard";
 import { useWindowSize } from "usehooks-ts";
 
@@ -5,7 +6,12 @@ export function Game() {
   const { height, width } = useWindowSize();
 
   return (
-    <div className="p-4 [&>div]:w-min! w-auto items-center h-auto flex bg-secondary text-lg font-semibold rounded-2xl justify-center">
+    <div
+      className={cn(
+        "p-4 w-auto items-center h-auto flex bg-secondary text-lg font-semibold rounded-2xl justify-center",
+        "[&>div]:w-min! [&>div]:rounded-lg [&>div]:overflow-clip"
+      )}
+    >
       <Chessboard
         arePiecesDraggable
         showBoardNotation
