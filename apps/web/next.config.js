a29ffbc7efe4d472565fdd3888952a5d1d46f108
Himanshu Mendapara <himanshumendapra@gmail.com>
2025-06-30 22:21:43 +0530

    fix: css loading issue

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ["@konekt/ui", "@konekt/tailwind-config"],
 };
 
 export default nextConfig;
